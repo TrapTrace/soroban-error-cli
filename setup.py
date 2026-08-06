a0ef11b7c75cddcb634de83092348d31c0b64046
Tim Oyelabi <oyelabitimothy@gmail.com>
2026-08-06 14:06:58 +0100

    build: add packaging configuration pyproject.toml and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="traptrace-cli",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "soroban-explain=traptrace_cli.cli:main",
+            "traptrace=traptrace_cli.cli:main",
+        ],
+    },
+    author="TrapTrace Team",
+    description="CLI error lookup and diagnostic tool for Stellar Soroban developers",
+    license="MIT",
+    keywords="stellar soroban cli error debugging",
+)
---
+++ traptrace_cli/__init__.py
@@ -0,0 +1,4 @@
+"""
+TrapTrace CLI - Soroban Error Lookup Tool
+"""
+__version__ = "0.1.0"
